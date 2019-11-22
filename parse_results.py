20a7f790825074e2d7f682d7724903273c95aff4
solashirai <solashakashirai@gmail.com>
2019-11-21 20:03:07 -0500

    tweaks

--- parse_results.py
+++ parse_results.py
@@ -13,7 +13,7 @@ def analysis_with_errorbars(res, ground_truth, save_file, alg_name, data_name):
 	fig, axes = plt.subplots(1, 2, figsize = (10, 5))
 	fig.suptitle("{} - {}".format(data_name, alg_name))
 
-	axes[0].plot([p for (p, cnt, cnt_std, t, t_std) in res], [cnt for (p, cnt, cnt_std, t, t_std) in res], '.-', color = 'k', label="simulation")
+	axes[0].plot([p for (p, cnt, cnt_std, t, t_std) in res], [cnt for (p, cnt, cnt_std, t, t_std) in res], '.-', color = 'k')
 	axes[0].errorbar([p for (p, cnt, cnt_std, t, t_std) in res], [cnt for (p, cnt, cnt_std, t, t_std) in res], yerr=[cnt_std for (p, cnt, cnt_std, t, t_std) in res], linestyle='none')
 	axes[0].axhline(y=ground_truth[0], linestyle='dotted', color='r')
 	#
@@ -21,16 +21,13 @@ def analysis_with_errorbars(res, ground_truth, save_file, alg_name, data_name):
 	axes[0].set_xlabel("p")
 	axes[0].set_ylabel("cnt.")
 
-	axes[1].plot([p for (p, cnt, cnt_std, t, t_std) in res], [t for (p, cnt, cnt_std, t, t_std) in res], '.-', color = 'k', label = "simulation")
+	axes[1].plot([p for (p, cnt, cnt_std, t, t_std) in res], [t for (p, cnt, cnt_std, t, t_std) in res], '.-', color = 'k')
 	axes[1].errorbar([p for (p, cnt, cnt_std, t, t_std) in res], [t for (p, cnt, cnt_std, t, t_std) in res], yerr=[t_std for (p, cnt, cnt_std, t, t_std) in res], linestyle='none')
 	axes[1].axhline(y=ground_truth[1], linestyle='dotted', color='r')
 	#
 	axes[1].set_title("Doulin Runtime")
 	axes[1].set_xlabel("p")
 	axes[1].set_ylabel("running time (sec.)")
-
-	for i in range(2):
-		axes[i].legend()
 
 	fig.savefig(save_file)
 
@@ -41,7 +38,7 @@ def accuracy_speedup_dots(res, ground_truth, save_file, alg_name, data_name):
 	:param res: [(p, cnt, running_time), ...]
 	:ground_truth: (cnt, running_time)
 	'''
-	res_accuracy = [1-np.abs((cnt/(p**3)/ground_truth[0] -1)) for (p, cnt, t) in res]
+	res_accuracy = [1-np.abs((cnt/(p**3)/ground_truth[0] - 1)) for (p, cnt, t) in res]
 	res_speedup = [ground_truth[1]/t for (p, cnt, t) in res]
 	plt.plot(res_accuracy, res_speedup, 'ro')
 	p = [0.1, 0.3, 0.5, 0.7, 1.0]
@@ -57,10 +54,19 @@ def accuracy_speedup_dots(res, ground_truth, save_file, alg_name, data_name):
 
 
 def main():
-	alg_name='Node Iterator'
+	# vis_sets = [
+	# 	('Node Iterator', 'HEP-TH-NEW', 'results/hep_th', 'node_iter/'),
+	# 	('Edge Iterator', 'HEP-TH-NEW', 'results/hep_th', 'edge_iter/'),
+	# 	('Node Iterator', 'EAT RS', 'results/EAT_RS', 'node_iter/'),
+	# 	('Edge Iterator', 'EAT RS', 'results/EAT_RS', 'edge_iter/'),
+	# 	('Node Iterator', 'Epinions', 'results/epinions', 'node_iter/'),
+	# 	('Edge Iterator', 'Epinions', 'results/epinions', 'edge_iter/')
+	# 			]
+	# for (alg_name, data_name, dataset, alg) in vis_sets:
+	alg_name='Edge Iterator'
 	data_name='HEP-TH-NEW'
 	dataset = 'results/hep_th/'
-	alg = 'node_iter/'
+	alg = 'edge_iter/'
 	file = dataset + alg + 'result.txt'
 	# savefile = dataset+alg+"result.png"
 
@@ -94,7 +100,7 @@ def main():
 	for p in [0.1, 0.3, 0.5, 0.7, 1.0]:
 		res.append((p, np.mean(p_count_dict[p])/(p**3), np.std(p_count_dict[p])/(p**3), np.mean(p_time_dict[p]), np.std(p_time_dict[p])))
 
-	analysis_with_errorbars(res, (res[-1][1], res[-1][3]), dataset+alg+"result_web.png", alg_name, data_name)
+	analysis_with_errorbars(res, (res[-1][1], res[-1][3]), dataset+alg+"result_with_errorbars.png", alg_name, data_name)
 
 if __name__ == '__main__':
 	main()
